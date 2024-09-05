512b1951efa60f186d80efc8d08457fb1c546dff
gerar24 <gerardonahuel@hotmail.com>
2024-09-05 10:09:54 -0300

    runs (upg failed 10 palo fail)

--- diezmil.py
+++ diezmil.py
@@ -206,7 +206,7 @@ def main():
         #("politica_picada.csv", ElBatoQueSoloCalculaPromediosPicados),
         #("politica_maspicada_10palos.csv", ElBatoQueSoloCalculaPromediosMasPicados),
         #("politica_picadaplus_1palo_2.5%_nmalrecomp.csv", ElBatoQueSoloCalculaPromediosPicadosPlus),
-        ("politica_maspicadaupgraded_1palo_2.5%.csv", ElBatoQueSoloCalculaPromediosMasPicados_Upgraded),
+        ("politica_maspicadaupgraded_10palo_5%.csv", ElBatoQueSoloCalculaPromediosMasPicados_Upgraded),
         # ("", AgenteQLearning),  # Añadir parámetros según sea necesario
     ]
 
@@ -215,9 +215,9 @@ def main():
 
         for j in tqdm(range(1), desc=f"Running {AgentClass.__name__}"):
             player_amounts = []
-            jugador = AgentClass(0.025, policy_file)
+            jugador = AgentClass(0.05, policy_file)
 
-            for i in tqdm(range(10000), desc=f"Iteration {j + 1}/10"):
+            for i in tqdm(range(10000000), desc=f"Iteration {j + 1}/10"):
                 juego = JuegoDiezMil(jugador)
                 (cantidad_turnos, puntaje_final) = juego.jugar(verbose=False)
                 player_amounts.append(cantidad_turnos)
@@ -246,9 +246,9 @@ def main():
         )
         plt.xlabel("Iteration")
         plt.ylabel("Average Play Amount")
-        plt.title(f"Average Play Amounts for {AgentClass.__name__} Over 1,000,000 Iterations")
+        plt.title(f"Average Play Amounts for {AgentClass.__name__} Over 10,000,000 Iterations")
         plt.text(len(average_play_amounts) * 0.75, np.min(average_play_amounts) * 0.98, f"Average in last 10000 iterations{overall_average}")
-        plt.savefig(f"Montecarlo_{AgentClass.__name__}_1palo_2.5%.png")
+        plt.savefig(f"Montecarlo_{AgentClass.__name__}_10palo_5%.png")
         plt.close()
 
 
